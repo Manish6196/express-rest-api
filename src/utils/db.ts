80540f2b4737b41106a44dd764338cfb5dfa9171
manish <manishrajput6196@gmail.com>
2024-03-25 17:46:47 +0530

    utils/db: add db connection logic

---
+++ src/utils/db.ts
@@ -0,0 +1,7 @@
+import { MONGO_URL } from '../config'
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+  await mongoose.connect(MONGO_URL)
+  console.log('Connected to DB!')
+}
